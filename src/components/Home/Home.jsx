c4b5ec19ce9f1336c956eacbb22542a19eab5bdf
lidor4x4 <lidor4x4@gmail.com>
2022-07-21 16:03:18 +0300

    fixed typo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,16 +3,14 @@ import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./css/Home.css";
 
-
 export default function Home() {
-
   return (
     <div className="home-container">
       <h1 className="title-home">Hello! Im Lidor</h1>
       <p className="desc-home">a full-stack web developer</p>
       <ul className="home-nav">
         <motion.li whileHover={{ scale: 1.1 }}>
-          <a className="home-nav-link" href="/about">
+          <a className="home-nav-link" href="/about-me">
             → About Me{" "}
           </a>
         </motion.li>
